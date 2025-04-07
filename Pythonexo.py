fec79db69de29758e60d9060558a74f404110e5c
Martin06270 <martinng06@hotmail.com>
2025-04-07 23:24:01 +0200

    exo suplementaire

--- Pythonexo.py
+++ Pythonexo.py
@@ -4,4 +4,10 @@ for i in range(0, n + 1):
     if i % 2 == 0:
         print(i)
 
+        n = int(input("Entrez un nombre entier positif : "))
+print(f"Nombres pairs de 0 à {n} :")
+for i in range(0, n + 1):
+    if i % 2 == 0:
+        print(i)
+
         
