f4d2a419398b240170b3493f380d52f43f266053
Martin06270 <martinng06@hotmail.com>
2025-05-27 21:33:25 +0200

    commantaire de code

--- Pythonexo.py
+++ Pythonexo.py
@@ -1,6 +1,10 @@
 n = int(input("Entrez un nombre entier positif : "))
+# Affiche les nombres pairs de 0 à n    
+
 print(f"Nombres pairs de 0 à {n} :")
-for i in range(0, n + 1):
+
+for i in range(0, n + 1)
+
     if i % 2 == 0:
         print(i)
 
